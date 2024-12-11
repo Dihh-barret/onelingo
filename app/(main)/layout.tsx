32baf4e27a29b24fc701e7b634cd36617092b0df
Diogo Barreto <diogobarreto@MacBook-Air-de-Diogo.local>
2024-12-11 17:47:02 -0300

    foi

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,9 +1,25 @@
-const LearnPage = () => {
+import { Sidebar } from "@/components/sidebar";
+import { MobileHeader } from "@/components/ui/mobile-header";
+import { Children } from "react";
+
+type props = {
+    children: React.ReactNode;
+}
+
+const MainLayout = ({
+    children,
+}: props) => {
     return (
-        <div>
-            Learn page
-        </div>
+        <>
+            <MobileHeader/>
+            <Sidebar className="hidden lg:flex"/>
+            <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
+                <div className="bg-red-500 h-full">
+                    {children} 
+                </div>                
+            </main>            
+        </>
     )
 }
 
-export default LearnPage;
+export default MainLayout
