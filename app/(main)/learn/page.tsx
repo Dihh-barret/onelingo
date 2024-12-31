afa74d816349d0b7a58060fdce5ca55a98255bc6
Diogo Barreto <diogo.s.teki@hotmail.com>
2024-12-31 17:20:54 -0300

    Learn page wrapper feito

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,15 +1,21 @@
-import { FeedWrapper } from "@/components/feed-wrapper";
-import { StickyWwrapper } from "@/components/Stick-wraper";
 import { Header } from "./header";
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { StickyWwrapper } from "@/components/stick-wraper";
 
 const LearnPage = () => {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
-      <StickyWwrapper>My stick Sidebar</StickyWwrapper>
+      <StickyWwrapper>
+        <UserProgress
+          activeCourse={{ title: "Portuguese", imageSrc: "/br.svg" }}
+          hearths={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWwrapper>
       <FeedWrapper>
-        <Header title="Portuguese">
-
-        </Header>
+        <Header title="Portuguese"></Header>
       </FeedWrapper>
     </div>
   );
--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,41 +1,36 @@
-import { ClerkLoaded, 
-        ClerkLoading,
-        SignedOut,
-        SignInButton, 
-        UserButton,
-        SignedIn,
-    SignIn} from "@clerk/nextjs";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  SignedIn,
+} from "@clerk/nextjs";
 import { Loader } from "lucide-react";
 import Image from "next/image";
 
-
 export const Header = () => {
-
-    return (
-        <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-            <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
-                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-                    <Image src="/duolingo icon.svg" height={40} width={40} alt="icon"/>
-                    <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
-                        Onelingo
-                    </h1>
-                </div>
-                <ClerkLoading>
-                    <Loader className="h-5 w-5 text-muted-foreground animate-spin">
-
-                    </Loader>
-                </ClerkLoading>
-                <ClerkLoaded>
-                    <SignedIn >
-                        <UserButton />
-                    </SignedIn>
-                    <SignedOut>
-                        <SignInButton fallbackRedirectUrl={"/learn"}>
-                            Login
-                        </SignInButton>
-                    </SignedOut>
-                </ClerkLoaded>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/duolingo icon.svg" height={40} width={40} alt="icon" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Onelingo
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin"></Loader>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton fallbackRedirectUrl={"/learn"}>Login</SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
+    </header>
+  );
+};
